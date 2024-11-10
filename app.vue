f026325cfbb5d1303c849e4a75ebbbac1adec6e7
hikarucraft <147622480+hikarucraft@users.noreply.github.com>
2024-11-10 09:14:42 +0900

    setup: completed sass setup

--- app.vue
+++ app.vue
@@ -2,5 +2,15 @@
   <div>
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
+    <h1 class="hoge">hogehgoe</h1>
   </div>
 </template>
+
+<style lang="scss">
+div{
+ background-color:$primary
+}
+.hoge{
+  color:$primary
+}
+</style>
